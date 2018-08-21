24bfba2207cc5e2ecb4656a80057dff78637cc81
tsudonymn <tsudonymn@gmail.com>
2018-08-20 23:42:20 -0400

    First

---
+++ import_from_wp.rb
@@ -0,0 +1,7 @@
+require "jekyll-import";
+JekyllImport::Importers::WordPress.run({
+    "dbname"   => "xtr1029211313849",
+    "user"     => "bn_wordpress",
+    "password" => "0f6a4f5b8b",
+    "host"     => "localhost",
+    "port"     => "3306"})
