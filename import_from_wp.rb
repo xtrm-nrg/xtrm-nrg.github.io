e1854fbcaede67765b2c3a0e4a9f87166495f616
Chris <tsudonymn@yahoo.com>
2018-08-21 00:36:50 -0400

    Delete import_from_wp.rb

--- import_from_wp.rb
+++
@@ -1,7 +0,0 @@
-require "jekyll-import";
-JekyllImport::Importers::WordPress.run({
-    "dbname"   => "xtr1029211313849",
-    "user"     => "bn_wordpress",
-    "password" => "0f6a4f5b8b",
-    "host"     => "localhost",
-    "port"     => "3306"})
